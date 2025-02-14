d32001f863a9dc263a4e1302464fa10c7fd585bc
xuguyGIT <xuguy970925@163.com>
2025-02-14 11:57:16 +0800

    dlwpt

--- pyTorchPrac.py
+++ pyTorchPrac.py
@@ -33,4 +33,35 @@ output = torch.addmm(input_tensor, mat1, mat2)
 output.backward(tmp_tensor)
 mat2.grad
 
+# ========= Deep-Learning with pytorch ch01
+# torchvision: torch for computer vision
+from torchvision import models
+from torchvision import transforms
+
+# check models list
+len(dir(models)) # total 221 models
+
+# load alex net
+resnet = models.resnet101(pretrained = True)
+resnet
+
+preprocess = transforms.Compose([transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+'''
+preprocess image:
+1. resize to 256x256
+2. crop image to 224x224 around the center
+3. transform it to a tensor
+4. 
+'''
+
+from PIL import Image
+# instead of using git clone, you can add 'ss' before the 'github.com' format file url to download
+img = Image.open('../dlwpt-code/data/p1ch2/bobby.jpg')
+
+
+
+
 
