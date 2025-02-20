e51c47e9b7cd2cd9cdfa8d466f24a61f7f79c962
xuguyGIT <xuguy970925@163.com>
2025-02-20 22:34:20 +0800

    add some comment wrt packaging

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -6,4 +6,7 @@ from dezero.core_simple import as_array
 from dezero.core_simple import as_variable
 from dezero.core_simple import setup_variable
 
+# 为什么用dezero.core_simple？这个__init__文件不是已经和core_simple.py在同一个目录下吗？
+# 因为我们总是从外部调用这个文件，我们会首先把dezero这个文件的父目录加入到全局变量中，这样我们就可以import dezero这个包。当我们import dezero后，__init__.py会被首先自动调用，但是这个时候，全局变量中保存的是dezero的父目录，因此__init__.py的import并不能识别到dezero内部的结构，因此需要从dezero的父目录开始寻找，所以需要dezero.core_simple，告诉import语句应该进入dezero包内的core_simple中import相应的模块
+
 setup_variable()
