31417828890551adcf95bc6f608f15dd95f2abc8
xuguyGIT <xuguy970925@163.com>
2025-02-22 22:35:10 +0800

    rewrite deezero core.py to adapt higher order gradients

--- DeZero.py
+++ DeZero.py
@@ -1482,17 +1482,28 @@ if '__file__' in globals():
 import numpy as np
 from dezero.utils import plot_dot_graph
 from dezero import Variable
+# from DL-dezero.core import Variable
 
 
 # verify graphviz
-def goldstein(x, y):
-    z = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2  - 14*y + 6*x*y + 3*y**2)) * (30 + (2*x- 3*y) **2 * (18- 32*x + 12*x**2 + 48*y-36*x*y + 27*y**2))
-    return z
+def f(x):
+    y = x**3
+    return y
 
 x = Variable(np.array([1.0]))
-y = Variable(np.array([1.0]))
-z = goldstein(x, y)
-z.backward()
+y = f(x)
+# create_graph = True ->启动反向传播
+y.backward(create_graph=False)
+print(x.grad)
+
+
+gx = x.grad
+x.cleargrad()
+gx.backward()
+print(x.grad)
+
+
+
 
 x.name = 'x'
 y.name = 'y'
